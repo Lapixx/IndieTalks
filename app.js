262f2e6a1d96df4dad09e7f6a45058bb4170ff2a
Tijn Kersjes <tijn@divbyzero.nl>
2015-04-27 21:29:15 +0200

    Added list view and route handlers

--- app.js
+++ app.js
@@ -1,14 +1,57 @@
 // dependencies
-var connect = require("connect");
+var fs = require("fs");
+var express = require("express");
 var serveStatic = require("serve-static");
+var Handlebars = require("handlebars");
 
-var app = connect();
+var video = fs.readFileSync("./video.html", {encoding: "UTF8"});
+var video_template = Handlebars.compile(video);
+var list = fs.readFileSync("./list.html", {encoding: "UTF8"});
+var list_template = Handlebars.compile(list);
+
+var routes = require("./routes").reverse();
+
+var app = express();
 
 app.use(function (req, res, next) {
     console.log(req.method, req.headers.host, req.url);
     next();
 });
 
+function makeVideoGetter(talk) {
+
+  var html = video_template(talk);
+
+  return function (req, res) {
+      res.send(html);
+      res.end();
+  }
+}
+
+function makeSlug(title) {
+  return title
+    .toLowerCase()
+    .replace(/ /g,'-')
+    .replace(/[^\w-]+/g,'');
+}
+
+
+for (var i = 0; i < routes.length; i++) {
+
+  if (routes[i].slug === undefined)
+    routes[i].slug = makeSlug(routes[i].title);
+
+  app.get("/" + routes[i].slug, makeVideoGetter(routes[i]));
+}
+
+var html = list_template({talks: routes});
+app.get("/", function (req, res) {
+
+  res.send(html);
+  res.end();
+});
+
+// static assets
 app.use(serveStatic("static"));
 
 // start listening
---
+++ routes.js
@@ -0,0 +1,124 @@
+module.exports = [
+
+  {
+    title: "Making Art Through Limitations",
+    speaker: {
+      name: "Jasper Byrne",
+      url: "#"
+    },
+    date: "25 april 2013",
+    location: "Indievelopment",
+    description: "Jasper Byrne giving his presentation \"Making Art Through Limitations\" at Indievelopment 2013. The second edition of the Indievelopment conference took place on April 25th, in Amsterdam, the Netherlands. Several people from the game industry shared their ideas about independent game development throughout the day.",
+    vimeo: "64917815"
+  },
+
+  {
+    title: "The Lie of the Grandfather Clock",
+    subtitle: "Searching for Alternatives to a Tempting Design",
+    speaker: {
+      name: "Jeroen D. Stout",
+      url: "#"
+    },
+    date: "25 april 2013",
+    location: "Indievelopment",
+    description: "Jeroen D. Stout giving his presentation \"The Lie of the Grandfather Clock: Searching for Alternatives to a Tempting Design\" at Indievelopment 2013. The second edition of the Indievelopment conference took place on April 25th, in Amsterdam, the Netherlands. Several people from the game industry shared their ideas about independent game development throughout the day.",
+    vimeo: "64925083"
+  },
+
+  {
+    title: "Advice to Running a Kickstarter of Your Own",
+    speaker: {
+      name: "Chris Avellone",
+      url: "#"
+    },
+    date: "25 april 2013",
+    location: "Indievelopment",
+    description: "Chris Avellone giving his presentation \"Advice to Running a Kickstarter of Your Own\" at Indievelopment 2013. The second edition of the Indievelopment conference took place on April 25th, in Amsterdam, the Netherlands. Several people from the game industry shared their ideas about independent game development throughout the day.",
+    vimeo: "65359054"
+  },
+
+  {
+    title: "Generate Everything",
+    speaker: {
+      name: "Michael Cook",
+      url: "#"
+    },
+    date: "25 april 2013",
+    location: "Indievelopment",
+    description: "Michael Cook giving his talk \"Generate Everything\" at Indievelopment 2014. The third edition of this independent game development conference took place on the 7th of April, at the Zijdebalen Theater in Utrecht, the Netherlands.",
+    vimeo: "92623463"
+  },
+
+  {
+    title: "Experimental Game Design... with Sound in Mind",
+    speaker: {
+      name: "Robin Hunicke & Austin Wintory",
+      url: "#"
+    },
+    date: "25 april 2013",
+    location: "Indievelopment",
+    description: "Robin Hunicke and Austin Wintory giving their presentation \"Experimental Game Design... with Sound in Mind\" at Indievelopment 2013. The second edition of the Indievelopment conference took place on April 25th, in Amsterdam, the Netherlands. Several people from the game industry shared their ideas about independent game development throughout the day.",
+    vimeo: "65359055"
+  },
+
+  {
+    title: "Never-Ending Audio: Generative Sound in Games",
+    speaker: {
+      name: "Paul Weir",
+      url: "#"
+    },
+    date: "7 april 2014",
+    location: "Indievelopment",
+    description: "Paul Weir giving his talk \"Never-Ending Audio: Generative Sound in Games\" at Indievelopment 2014. The third edition of this independent game development conference took place on the 7th of April, at the Zijdebalen Theater in Utrecht, the Netherlands.",
+    vimeo: "91805298"
+  },
+
+  {
+    title: "Generate Everything",
+    speaker: {
+      name: "Michael Cook",
+      url: "#"
+    },
+    date: "7 april 2014",
+    location: "Indievelopment",
+    description: "Michael Cook giving his talk \"Generate Everything\" at Indievelopment 2014. The third edition of this independent game development conference took place on the 7th of April, at the Zijdebalen Theater in Utrecht, the Netherlands.",
+    vimeo: "92623463"
+  },
+
+  {
+    title: "Coming of Age: Evolution of an Art Style",
+    speaker: {
+      name: "Clemens Scott",
+      url: "#"
+    },
+    date: "7 april 2014",
+    location: "Indievelopment",
+    description: "Clemens Scott giving his talk \"Coming of Age: Evolution of an Art Style\" at Indievelopment 2014. The third edition of this independent game development conference took place on the 7th of April, at the Zijdebalen Theater in Utrecht, the Netherlands.",
+    vimeo: "93066039"
+  },
+
+  {
+    title: "Attempting Deep Work",
+    speaker: {
+      name: "Jonathan Blow",
+      url: "#"
+    },
+    date: "7 april 2014",
+    location: "Indievelopment",
+    description: "Jonathan Blow giving his talk \"Attempting Deep Work\" at Indievelopment 2014. The third edition of this independent game development conference took place on the 7th of April, at the Zijdebalen Theater in Utrecht, the Netherlands.",
+    vimeo: "94259578"
+  },
+
+  {
+    title: "Ten Tips to Start your Indie Company Today",
+    speaker: {
+      name: "JP van Seventer",
+      company: "Dutch Game Garden",
+      url: "#"
+    },
+    date: "7 april 2014",
+    location: "Indievelopment",
+    description: "JP van Seventer, development director at the Dutch Game Garden, shares some insights on what students can do to start a great games company that will be successful and last forever. The third edition of this independent game development conference took place on the 7th of April, at the Zijdebalen Theater in Utrecht, the Netherlands.",
+    vimeo: "98279172"
+  }
+];
